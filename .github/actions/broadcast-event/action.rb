19a2e0eb6a5404251dde14fab8d9f646f28ae96f
Raskolnikov <jerry.tann@endress.com>
2022-03-21 13:54:12 +0800

    ✨ initial commit

---
+++ .github/actions/broadcast-event/action.rb
@@ -0,0 +1,27 @@
+require 'octokit'
+
+event = ENV['INPUT_EVENT']
+org = ENV['GITHUB_REPOSITORY'].split('/').first
+
+client = Octokit::Client.new(access_token: ENV['INPUT_TOKEN'], per_page: 100)
+client.auto_paginate = true
+
+payload = {
+  headers: { accept: 'application/vnd.github.v3+json' },
+  query: {
+    type: 'private'
+  }
+}
+
+repos_to_notify = client.organization_repositories(org, payload).select{|repo| repo.archived == false}.map(&:full_name)
+
+payload = {
+  event_type: event
+}
+
+repos_to_notify.each do |repo|
+  puts "Notifying #{repo} with event #{event}..."
+  client.post("/repos/#{repo}/dispatches", payload)
+rescue Octokit::NotFound => e
+  puts "For #{repo} the following error occurred: #{e}"
+end
---
+++ README.md
@@ -0,0 +1,3 @@
+# testing out workflow dispatch
+
+- this repository contains a github action written in ruby with octokit
