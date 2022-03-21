8cf71d0c3d27e2ce39bd4b35d80a43f1241209a6
Raskolnikov <jerry.tann@endress.com>
2022-03-21 14:40:26 +0800

    📝 modify action for testing purposes

--- .github/actions/broadcast-event/action.rb
+++ .github/actions/broadcast-event/action.rb
@@ -2,7 +2,7 @@ require 'octokit'
 
 event = ENV['INPUT_EVENT']
 # org = ENV['GITHUB_REPOSITORY'].split('/').first
-org = "jerry81/actions-practice"
+repo = "jerry81/actions-practice"
 client = Octokit::Client.new(access_token: ENV['INPUT_TOKEN'], per_page: 100)
 client.auto_paginate = true
 
@@ -13,15 +13,17 @@ payload = {
   }
 }
 
-repos_to_notify = client.organization_repositories(org, payload).select{|repo| repo.archived == false}.map(&:full_name)
+# repos_to_notify = client.organization_repositories(org, payload).select{|repo| repo.archived == false}.map(&:full_name)
 
 payload = {
   event_type: event
 }
 
-repos_to_notify.each do |repo|
-  puts "Notifying #{repo} with event #{event}..."
-  client.post("/repos/#{repo}/dispatches", payload)
-rescue Octokit::NotFound => e
-  puts "For #{repo} the following error occurred: #{e}"
-end
+client.post("/repos/#{repo}/dispatches", payload)
+
+# repos_to_notify.each do |repo|
+#   puts "Notifying #{repo} with event #{event}..."
+#   client.post("/repos/#{repo}/dispatches", payload)
+# rescue Octokit::NotFound => e
+#   puts "For #{repo} the following error occurred: #{e}"
+# end
